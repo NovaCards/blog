418a4e7614ca359445854a8e294673431ba808b3
charbelmarche33 <cmarche@mail.umw.edu>
2024-07-25 18:45:29 -0400

    Fixed article image sizing

--- src/components/posts/PostBody.jsx
+++ src/components/posts/PostBody.jsx
@@ -4,7 +4,7 @@ const PostBody = ({ post }) => {
   const { content } = post.fields
 
   return (
-    <div className='mx-auto !text-white'>
+    <div id="post-body" className='mx-auto !text-white'>
       <RichText content={content} />
     </div>
   )
