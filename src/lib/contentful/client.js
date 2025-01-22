e673ce7430fb9e1c74a6c2c01cbe7afc37e6764f
charbelmarche33 <cmarche@mail.umw.edu>
2025-01-22 09:30:14 -0500

    Edited client file

--- src/lib/contentful/client.js
+++ src/lib/contentful/client.js
@@ -9,4 +9,4 @@ export const previewClient = contentful.createClient({
   host: 'preview.contentful.com',
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
-})
+}) 
